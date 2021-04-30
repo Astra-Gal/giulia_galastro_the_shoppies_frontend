601f821614d38ac707867bf6544aa6c8ccfbdc52
Astra-Gal <giulia.galastro@gmail.com>
2021-04-30 16:09:34 +0100

    renamed variable

--- src/containers/MovieContainer.js
+++ src/containers/MovieContainer.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import MovieList from '../components/MovieList';
+import SearchBar from '../components/SearchBar';
 // import Request from "../helpers/request";
 
 const MovieContainer = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [query, setQuery] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+  const [nominees, setNominees] = useState([]);
+
+  const handleNomination = (movie) => {
+    const newNomineesList = [...nominees, movie];
+    setNominees(newNomineesList);
+  };
 
   useEffect(() => {
     setLoading(true);
 
     fetch(
-      `http://www.omdbapi.com/?s=${query}&apikey=${process.env.REACT_APP_OMDB_KEY}`,
+      `http://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.REACT_APP_OMDB_KEY}`,
     )
       .then((res) => res.json())
       .then((data) => {
@@ -27,15 +34,16 @@ const MovieContainer = () => {
         setError(message);
         setLoading(false);
       });
-  }, [query]);
+  }, [searchValue]);
 
   // if (loading) {
-  //   return <p>Loading...</p>;
+  //   return <p>Loading...searchValue
   // }
 
   return (
     <>
-      <MovieList movies={movies} />{' '}
+      <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+      <MovieList movies={movies} />
     </>
   );
 };
