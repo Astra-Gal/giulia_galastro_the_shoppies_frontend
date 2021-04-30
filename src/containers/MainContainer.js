837ed735ff9d1ff59d774a38e21b6a8b5d6dc967
Astra-Gal <giulia.galastro@gmail.com>
2021-04-30 16:10:45 +0100

    moved searchBar

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Title from '../components/Title';
-import SearchBar from '../components/SearchBar';
 import MovieContainer from './MovieContainer';
 
 const MainContainer = () => {
   return (
     <>
       <Title />
-      <SearchBar />
       <MovieContainer />
     </>
   );
