b747ce4365c499b7e795037de60dbaabf0100dad
Astra-Gal <giulia.galastro@gmail.com>
2021-05-04 17:04:20 +0100

    filter out movies with no posters

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './MovieList.css';
 
-const MovieList = ({ movies, handleNomination }) => {
+const MovieList = ({ movies }) => {
+  // ,handleNomination
   if (!movies) {
     return <p>Loading...</p>;
   }
+  // filter movies to remove any that don't have link to a poster
+  const moviesWithPosters = movies.filter((movie) => {
+    return movie.Poster.indexOf('http') !== -1;
+  });
 
-  const movieNodes = movies.map((movie, index) => {
+  const movieNodes = moviesWithPosters.map((movie, index) => {
     return (
       <li key={index}>
         <div className="image-wrapper">
           <img src={movie.Poster} alt={movie.Title} />
-          <button onClick={handleNomination}>Nominate</button>
+          <div className="nominate-wrapper">Nominate</div>
         </div>
       </li>
     );
