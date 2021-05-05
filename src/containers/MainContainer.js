a3055403db1b116b6997ef25163e3ea108310a16
Astra-Gal <giulia.galastro@gmail.com>
2021-05-05 16:24:36 +0100

    Added react-router-dom

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 import Title from '../components/Title';
+import About from '../components/About';
+import MyNoms from '../components/MyNoms';
 import MovieContainer from './MovieContainer';
 
 const MainContainer = () => {
   return (
-    <div>
-      <Title />
-      <MovieContainer />
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Title />
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/my-nominations">
+            <MyNoms />
+          </Route>
+          <Route path="/">
+            <MovieContainer />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
