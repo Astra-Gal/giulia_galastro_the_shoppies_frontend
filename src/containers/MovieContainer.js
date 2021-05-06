0f86a974e4746e2939e97a7508af353c594d5482
Astra-Gal <giulia.galastro@gmail.com>
2021-05-06 18:02:40 +0100

    imported GotToFive banner component

--- src/containers/MovieContainer.js
+++ src/containers/MovieContainer.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import MovieList from '../components/MovieList';
 import SearchBar from '../components/SearchBar';
-// import Request from "../helpers/request";
+import GotToFive from '../components/GotToFive';
 
 const MovieContainer = ({
   movies,
+  nominees,
   handleNomination,
   searchValue,
   setSearchValue,
   loading,
 }) => {
-  return (
-    <>
-      <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
-      <MovieList
-        movies={movies}
-        handleNomination={handleNomination}
-        loading={loading}
-      />
-    </>
-  );
+  if (nominees.length <= 4) {
+    return (
+      <>
+        <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+        <MovieList
+          movies={movies}
+          handleNomination={handleNomination}
+          loading={loading}
+        />
+      </>
+    );
+  }
+  if (nominees.length === 5) {
+    return <GotToFive />;
+  }
 };
 
 export default MovieContainer;
