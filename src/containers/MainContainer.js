9fc34ce47fb699ff86458d3a9ab0fe36d1a13bf2
Astra-Gal <giulia.galastro@gmail.com>
2021-05-06 12:25:13 +0100

    moved fetch from movieContainer to mainContainer

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Title from '../components/Title';
@@ -7,6 +7,43 @@ import MyNoms from '../components/MyNoms';
 import MovieContainer from './MovieContainer';
 
 const MainContainer = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [searchValue, setSearchValue] = useState('');
+  const [nominees, setNominees] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(
+      `http://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.REACT_APP_OMDB_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.Response === 'False') {
+          setError(data.Error);
+        } else {
+          setMovies(data.Search);
+        }
+        setLoading(false);
+      })
+      .catch(({ message }) => {
+        setError(message);
+        setLoading(false);
+      });
+  }, [searchValue]);
+
+  const handleNomination = (movie) => {
+    const newNomineesList = [...nominees, movie];
+    // if nominees.length <= 4
+    setNominees(newNomineesList);
+    console.log(nominees);
+  };
+
+  // I'll need a handleDeleteNomination function, and
+  //that needs to get passed to MyNoms
+
   return (
     <Router>
       <div>
@@ -18,10 +55,20 @@ const MainContainer = () => {
             <About />
           </Route>
           <Route path="/my-nominations">
-            <MyNoms />
+            <MyNoms
+              nominees={nominees}
+              // handleNominationDelete
+              loading={loading}
+            />
           </Route>
           <Route path="/">
-            <MovieContainer />
+            <MovieContainer
+              movies={movies}
+              handleNomination={handleNomination}
+              searchValue={searchValue}
+              setSearchValue={setSearchValue}
+              loading={loading}
+            />
           </Route>
         </Switch>
       </div>
--- src/containers/MovieContainer.js
+++ src/containers/MovieContainer.js
@@ -1,51 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import MovieList from '../components/MovieList';
 import SearchBar from '../components/SearchBar';
 // import Request from "../helpers/request";
 
-const MovieContainer = () => {
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [searchValue, setSearchValue] = useState('');
-  const [nominees, setNominees] = useState([]);
-
-  const handleNomination = (movie) => {
-    const newNomineesList = [...nominees, movie];
-    setNominees(newNomineesList);
-    console.log(nominees);
-  };
-
-  useEffect(() => {
-    setLoading(true);
-
-    fetch(
-      `http://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.REACT_APP_OMDB_KEY}`,
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.Response === 'False') {
-          setError(data.Error);
-        } else {
-          setMovies(data.Search);
-        }
-        setLoading(false);
-      })
-      .catch(({ message }) => {
-        setError(message);
-        setLoading(false);
-      });
-  }, [searchValue]);
-
-  // if (loading) {
-  //   return <p>Loading...searchValue
-  // }
-
+const MovieContainer = ({
+  movies,
+  handleNomination,
+  searchValue,
+  setSearchValue,
+  loading,
+}) => {
   return (
     <>
       <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
-      <MovieList movies={movies} />
-      {/* handleNomination={handleNomination} */}
+      <MovieList
+        movies={movies}
+        handleNomination={handleNomination}
+        loading={loading}
+      />
     </>
   );
 };
