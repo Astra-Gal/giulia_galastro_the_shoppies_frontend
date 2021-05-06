4cb9c9d4da827a3cd4aff5c63b667fa5e7ed516e
Astra-Gal <giulia.galastro@gmail.com>
2021-05-06 18:02:57 +0100

    start of GotToFive banner notification

---
+++ src/components/GotToFive.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './GotToFive.css';
+
+const GotToFive = () => {
+  return (
+    <div className="gotToFive">
+      <h2>Got to five!</h2>
+      {/* Link to my noms */}
+    </div>
+  );
+};
+
+export default GotToFive;
