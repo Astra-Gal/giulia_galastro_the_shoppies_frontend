0de95f34dd98e7b1464615ad1aab9fd54497d6c0
Astra-Gal <giulia.galastro@gmail.com>
2021-05-06 18:36:03 +0100

    Styling & adding svg stars to GotToFive

--- src/components/GotToFive.js
+++ src/components/GotToFive.js
@@ -5,8 +5,142 @@ import './GotToFive.css';
 const GotToFive = () => {
   return (
     <div className="gotToFive">
-      <h2>Got to five!</h2>
-      {/* Link to my noms */}
+      <h2>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          width="40px"
+          height="40px"
+          viewBox="0 0 450 100"
+        >
+          <polygon
+            fill="black"
+            stroke="black"
+            points="279,5 294,35 328,40 303,62 309,94 279,79 248,94 254,62 230,39 263,35"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          width="40px"
+          height="40px"
+          viewBox="0 0 450 100"
+        >
+          <polygon
+            fill="black"
+            stroke="black"
+            points="279,5 294,35 328,40 303,62 309,94 279,79 248,94 254,62 230,39 263,35"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          width="40px"
+          height="40px"
+          viewBox="0 0 450 100"
+        >
+          <polygon
+            fill="black"
+            stroke="black"
+            points="279,5 294,35 328,40 303,62 309,94 279,79 248,94 254,62 230,39 263,35"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          width="40px"
+          height="40px"
+          viewBox="0 0 450 100"
+        >
+          <polygon
+            fill="black"
+            stroke="black"
+            points="279,5 294,35 328,40 303,62 309,94 279,79 248,94 254,62 230,39 263,35"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          width="40px"
+          height="40px"
+          viewBox="0 0 450 100"
+        >
+          <polygon
+            fill="black"
+            stroke="black"
+            points="279,5 294,35 328,40 303,62 309,94 279,79 248,94 254,62 230,39 263,35"
+          />
+        </svg>
+        Got to five!
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          width="40px"
+          height="40px"
+          viewBox="0 0 450 100"
+        >
+          <polygon
+            fill="black"
+            stroke="black"
+            points="279,5 294,35 328,40 303,62 309,94 279,79 248,94 254,62 230,39 263,35"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          width="40px"
+          height="40px"
+          viewBox="0 0 450 100"
+        >
+          <polygon
+            fill="black"
+            stroke="black"
+            points="279,5 294,35 328,40 303,62 309,94 279,79 248,94 254,62 230,39 263,35"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          width="40px"
+          height="40px"
+          viewBox="0 0 450 100"
+        >
+          <polygon
+            fill="black"
+            stroke="black"
+            points="279,5 294,35 328,40 303,62 309,94 279,79 248,94 254,62 230,39 263,35"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          width="40px"
+          height="40px"
+          viewBox="0 0 450 100"
+        >
+          <polygon
+            fill="black"
+            stroke="black"
+            points="279,5 294,35 328,40 303,62 309,94 279,79 248,94 254,62 230,39 263,35"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          width="40px"
+          height="40px"
+          viewBox="0 0 450 100"
+        >
+          <polygon
+            fill="black"
+            stroke="black"
+            points="279,5 294,35 328,40 303,62 309,94 279,79 248,94 254,62 230,39 263,35"
+          />
+        </svg>
+      </h2>
+      <Link to="/my-nominations" className="nomLink">
+        See or edit your nominations <span> &#10154;</span>
+      </Link>
     </div>
   );
 };
