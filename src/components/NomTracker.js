fcd8ccb7712d876d1b5683e493baa2651b1bfda2
Astra-Gal <giulia.galastro@gmail.com>
2021-05-08 14:56:16 +0100

    nom banner instructions improved

--- src/components/NomTracker.js
+++ src/components/NomTracker.js
@@ -5,14 +5,20 @@ const NomTracker = ({ nominees }) => {
   if (nominees.length === 0) {
     return (
       <div className="NomTracker">
-        <h4>Nommy nommy nom nom</h4>
+        <h4>Please nominate 5 movies</h4>
       </div>
     );
-  } else if (nominees.length <= 4) {
+  } else if (nominees.length < 4) {
     const nomsRemaining = 5 - nominees.length;
     return (
       <div className="NomTracker">
         <h4>You have {nomsRemaining} nominations remaining</h4>
+      </div>
+    );
+  } else if (nominees.length === 4) {
+    return (
+      <div className="NomTracker">
+        <h4>You have 1 nomination remaining</h4>
       </div>
     );
   } else {
