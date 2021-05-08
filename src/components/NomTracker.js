68d576eae8d81d3ac3bab57c8ee0e9979ca965d0
Astra-Gal <giulia.galastro@gmail.com>
2021-05-08 20:51:41 +0100

    Corrected heading level for NomTracker banner

--- src/components/NomTracker.js
+++ src/components/NomTracker.js
@@ -5,20 +5,20 @@ const NomTracker = ({ nominees }) => {
   if (nominees.length === 0) {
     return (
       <div className="NomTracker">
-        <h4>Please nominate 5 movies</h4>
+        <h2>Please nominate 5 movies</h2>
       </div>
     );
   } else if (nominees.length < 4) {
     const nomsRemaining = 5 - nominees.length;
     return (
       <div className="NomTracker">
-        <h4>You have {nomsRemaining} nominations remaining</h4>
+        <h2>You have {nomsRemaining} nominations remaining</h2>
       </div>
     );
   } else if (nominees.length === 4) {
     return (
       <div className="NomTracker">
-        <h4>You have 1 nomination remaining</h4>
+        <h2>You have 1 nomination remaining</h2>
       </div>
     );
   } else {
