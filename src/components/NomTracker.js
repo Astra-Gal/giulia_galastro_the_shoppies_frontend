6adb0466de85ea93958bf953aa45ef2055befda3
Astra-Gal <giulia.galastro@gmail.com>
2021-05-08 21:16:09 +0100

    changed from heading into p tags

--- src/components/NomTracker.js
+++ src/components/NomTracker.js
@@ -5,20 +5,20 @@ const NomTracker = ({ nominees }) => {
   if (nominees.length === 0) {
     return (
       <div className="NomTracker">
-        <h2>Please nominate 5 movies</h2>
+        <p>Please nominate 5 movies</p>
       </div>
     );
   } else if (nominees.length < 4) {
     const nomsRemaining = 5 - nominees.length;
     return (
       <div className="NomTracker">
-        <h2>You have {nomsRemaining} nominations remaining</h2>
+        <p>You have {nomsRemaining} nominations left</p>
       </div>
     );
   } else if (nominees.length === 4) {
     return (
       <div className="NomTracker">
-        <h2>You have 1 nomination remaining</h2>
+        <p>You have 1 nomination left</p>
       </div>
     );
   } else {
